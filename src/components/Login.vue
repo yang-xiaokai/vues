32b2c04ffe3b19d346a9759a14037c236edc62b8
yangxiaokai <1882120@163.com>
2021-03-11 16:51:03 +0800

    首次提交

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    
+<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" >
+  <el-form-item label="用户名" prop="name">
+    <el-input v-model="ruleForm.name" ></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop="password">
+    <el-input v-model="ruleForm.password"></el-input>
+  </el-form-item>
+ 
+  
+</el-form>
+</template>
+import axios from "../api/common";
+<script>
+
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          
+          name: '',
+          password:''
+
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'change' }
+          ]
+          
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
